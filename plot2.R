7ef72c04780671eb4e4a0d82abd0621dc1e1f376
eryk orłowski <eof@offline.pl>
2017-04-30 11:29:43 +0200

    old version was uploaded :/

--- plot2.R
+++ plot2.R
@@ -1,28 +1,40 @@
-
 library(sqldf)
 setwd("~/Documents/r/coursera/04 exploratory data analysis/week 1/")
-d0 <- read.csv.sql(file="household_power_consumption.txt", sep=";", sql="select * from file where Date='1/2/2007' or Date='2/2/2007'")
 
+##----Select only two days from file----
+d0 <- read.csv.sql(
+  file="household_power_consumption.txt", 
+  sep=";", 
+  sql="select * from file where Date='1/2/2007' or Date='2/2/2007'"
+)
+
+##----Review data----
 head(d0)
 str(d0)
-
-d0$Date <- as.Date(d0$Date, "%d/%m/%Y" )
-?strptime
-strptime(d0$Time, "%H:%M:%S")
-
 summary(d0)
 
-png(file="plot2.png")
-xrange=range(d0$Date)
-plot(d0$Global_active_power,
+##----Merge Date and Time columns into separate date-time col----
+d0$date.time <- as.POSIXct(
+  paste(d0$Date, d0$Time), 
+  format="%d/%m/%Y %H:%M:%S"
+)
+
+##----Set local machine timezone to US, to get proper x-labeling----
+curr_locale <- Sys.getlocale("LC_TIME")
+Sys.setlocale("LC_TIME","en_US")
+
+##----Main plotting function----
+png(file="plot2.png", width=480, height=480)
+plot(d0$date.time,
+     d0$Global_active_power,
      type="l",
      main="",
      xlab="",
      ylab="Global Active Power (kilowatts)"
-     )
-axis(1,labels = c("Thu", "Fri", "Sat"), at=c(1,1500,3000) )
+)
 
 dev.off()
 
-
+##----Set timezone back to default----
+Sys.setlocale("LC_TIME",curr_locale)
 
